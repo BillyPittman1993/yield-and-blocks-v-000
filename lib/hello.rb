cfbb26bcbffb61b6f8f9812478df29581e27e39b
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-25 12:31:26 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,5 @@
-["Tim", "Tom", "Jim"].each do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
+def yielding
+  puts "the program is executing the code inside the method"
+  yield
+  puts "now we are back in the method"
 end
