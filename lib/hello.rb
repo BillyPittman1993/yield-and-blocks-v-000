d2c005a4297bef8cebbe6dbac82c1c6f95226567
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-25 12:39:47 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,5 @@
-def yielding_with_arguments(num)
-  puts "the program is executing the code inside the method"
-  yield(num)
-  puts "now we are back in the method"
+["Tim", "Tom", "Jim"].each do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
 end
-
-yielding_with_arguments(2) { |i| puts i * 2 }
