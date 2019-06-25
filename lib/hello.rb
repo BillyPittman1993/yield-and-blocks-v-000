d381e92840e16fcffe2acea1c1f9b582ddc6bd52
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-25 12:35:48 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,4 +4,4 @@ def yielding_with_arguments(num)
   puts "now we are back in the method"
 end
 
-yielding { puts "the method has yielded to the block!" }
+yielding_with_arguments(num) { puts "the method has yielded to the block!" }
